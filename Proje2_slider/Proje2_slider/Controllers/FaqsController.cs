e0f966be7e649d95bc963eade80c1ee8774e5db7
YusufTahaKasdemir <yusufkasdemir870@gmail.com>
2023-12-26 22:22:19 +0300

    BusinessWebSite
    Using ASP.NET-MVC for website

---
+++ Proje2_slider/Proje2_slider/Controllers/FaqsController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Proje2_slider.Faqs
+{
+    public class FaqsController : Controller
+    {
+        // GET: Faqs
+        public ActionResult Faqs()
+        {
+            return View();
+        }
+    }
+}
---
+++ Proje2_slider/Proje2_slider/Controllers/HomeController.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Proje2_slider.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Main()
+        {
+            return View();
+        }
+
+        public ActionResult Services()
+        {
+            return View();
+        }
+
+        public ActionResult Projects()
+        {
+            return View();
+        }
+
+        public ActionResult Infos()
+        {
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            return View();
+        }
+
+        public ActionResult Footer()
+        {
+            return View();
+        }
+
+       
+
+      
+    }
+}
